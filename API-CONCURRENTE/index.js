e3562a87d5f7c5294112db76f575a02dde80c0b6
DANIEL-MAR-CZ <64609948+DANIEL-MAR-CZ@users.noreply.github.com>
2020-11-18 23:48:23 -0600

    Update index.js

--- API-CONCURRENTE/index.js
+++ API-CONCURRENTE/index.js
@@ -9,7 +9,7 @@ const mongoose = require('mongoose')
 const Product = require('./models/product.js')
 
 const app= express()
-const port = process.env.PORT || 3009
+const port = process.env.PORT || 3003
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -84,7 +84,7 @@ app.delete('/api/botones/:botonesId',(req, res) => {
 
 //mongoose.connect('mongodb://localhost:27017/apagador', (err,res) =>{
     
-mongoose.connect('mongodb://localhost:27020/apagador', { 'useNewUrlParser': true, 'useUnifiedTopology': true }, (err) =>{
+mongoose.connect('mongodb://localhost:27017/apagador', { 'useNewUrlParser': true, 'useUnifiedTopology': true }, (err) =>{
    if(err){
      return console.log(`ERRROR al conectar a la base de datos: ${err}`)
     }
